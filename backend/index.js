0bb410c59135937fda14b8641a9584b6a877f5d2
Dev Rajwadi <devrajwadi11@gmail.com>
2025-05-28 22:59:33 +0530

    Update

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000", // for local development
-      "https://movies-watchlist-ten.vercel.app/", // replace with your actual Vercel URL
+      "https://vercel.com/devrajwadis-projects/movies-watchlist/5dSyeDyZg5GcpkfStKBuH1UyhBtr", // replace with your actual Vercel URL
       "https://movies-watchlist.vercel.app", // another possible URL format
       "https://devrajwadi-movies-watchlist.vercel.app", // based on your GitHub username
     ],
